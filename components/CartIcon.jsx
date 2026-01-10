cbc6e93d55d60c37f218ade02534bf75913a2ee0
Dwivedi Priya <pd7668665@gmail.com>
2026-01-11 01:45:48 +0530

    cart

--- components/CartIcon.jsx
+++ components/CartIcon.jsx
@@ -4,8 +4,14 @@ import React from "react";
 const CartIcon = () => {
   return (
     <div className="flex items-center gap-4">
-      <div className="relative w-8 h-8 md:w-5 md:h-5 bg-yellow-400">
-        <Image src='/cart.png' alt="cart" fill/>
+      <div className="relative w-8 h-8 md:w-6 md:h-6">
+        <Image
+          src="/cart.png"
+          alt="cart"
+          fill
+          sizes="(max-width: 768px) 32px, 24px"
+          className="object-contain"
+        />
       </div>
       <span>Cart (3)</span>
     </div>
