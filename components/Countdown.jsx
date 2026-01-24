6c8ce3be2171a00c6c6336826bf14ecc777481e0
Dwivedi Priya <pd7668665@gmail.com>
2026-01-24 22:16:17 +0530

    countdown-renewed

--- components/Countdown.jsx
+++ components/Countdown.jsx
@@ -1,33 +1,34 @@
-"use client";
-import React, { useState, useEffect } from "react";
+'use client'
+import React, { useEffect, useState } from 'react'
 
-const CountDown = () => {
-  const targetDate = new Date("2026-01-17T00:00:00").getTime();
 
-  const [timeLeft, setTimeLeft] = useState(
-    targetDate - Date.now()
-  );
+const Countdown = () => {
+  const targetDate=new Date("2026-03-17T00:00:00").getTime()
+  const [counter,setCounter]=useState(
+    targetDate-Date.now()
+  )
 
-  useEffect(() => {
-    if (timeLeft <= 0) return;
+  useEffect(()=>{
+    if(counter <= 0) return;
 
-    const timer = setInterval(() => {
-      setTimeLeft((prev) => prev - 1000);
-    }, 1000);
+    const timer=setInterval(()=>{
+      setCounter((prev)=>prev-1000)
+    },1000)
 
-    return () => clearInterval(timer);
-  }, [timeLeft]);
+    return clearInterval(timer);
+  },[counter])
 
-  const d = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const h = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-  const m = Math.floor((timeLeft / (1000 * 60)) % 60);
-  const s = Math.floor((timeLeft / 1000) % 60);
+  //Conversion from miliseconds to readable format
+  const d = Math.floor(counter / (1000 * 60 * 60 * 24));
+  const h = Math.floor((counter / (1000 * 60 * 60)) % 24);
+  const m = Math.floor((counter / (1000 * 60)) % 60);
+  const s = Math.floor((counter / 1000) % 60);
 
   return (
     <span className="font-bold text-5xl text-yellow-300">
-      {d}d : {h}h : {m}m : {s}s
-    </span>
-  );
-};
+       {d}d : {h}h : {m}m : {s}s 
+    </span> 
+  )
+}
 
-export default CountDown;
+export default Countdown
