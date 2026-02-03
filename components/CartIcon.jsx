2d346e119b3c8c821661f0003136f6502812fa98
Dwivedi Priya <pd7668665@gmail.com>
2026-02-03 10:09:15 +0530

    cart

--- components/CartIcon.jsx
+++ components/CartIcon.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const CartIcon = () => {
   return (
-    <div className="flex items-center gap-2">
+    <Link href={'/cart'} className="flex items-center gap-2">
       <div className="relative w-8 h-8 md:w-6 md:h-6">
         <Image
           src="/cart.png"
@@ -14,7 +15,7 @@ const CartIcon = () => {
         />
       </div>
       <span>Cart (3)</span>
-    </div>
+    </Link>
   );
 };
 
